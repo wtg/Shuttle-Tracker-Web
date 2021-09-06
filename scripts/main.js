2040836f1da0403e6f9f4ec8a33eaf0d4008fbcd
Gabriel Jacoby-Cooper <gerzersoftware@icloud.com>
2021-09-05 20:25:51 -0400

    Make significant improvements

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 import API from "./api.js"
 
+async function updateBuses() {
+	const busAnnotations = await api.readBuses()
+	const existingBusAnnotations = map.annotations.filter(annotation => {
+		return annotation.title.indexOf("Bus") == 0
+	})
+	map.removeAnnotations(existingBusAnnotations)
+	busAnnotations.forEach(busAnnotation => {
+		map.addAnnotation(busAnnotation)
+	})
+}
+
 const prefersDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
 const bootstrapDarkmode = window['bootstrap-darkmode']
 const themeConfig = new bootstrapDarkmode.ThemeConfig()
@@ -7,6 +18,15 @@ themeConfig.loadTheme = () => {
 	return prefersDarkMode ? "dark" : "light"
 };
 themeConfig.initTheme()
+
+const osesWithApp = ["Android", "Android-x86", "Chromium OS", "iOS", "Mac OS"]
+const parser = new UAParser()
+const result = parser.getResult()
+if (osesWithApp.includes(result.os.name)) {
+	const alert = document.getElementById("alert-app")
+	alert.classList.remove("invisible")
+}
+
 const tokenID = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ilg0M0szUjk0VDIifQ.eyJpc3MiOiJTWUJMSDI3N05GIiwiaWF0IjoxNjMwODU3NTYxLCJleHAiOjE2NjIzMzYwMDB9.ksSxblUOJECqgDaBlPU-VZsXG-UGI-W8ty8raDkD_eG2q6cW79-3EoDCHj-wHmynXRmDWR6Yfg72xknMS--ZdA"
 mapkit.init({
 	authorizationCallback: function(done) {
@@ -30,17 +50,5 @@ const stopAnnotations = await api.readStops()
 stopAnnotations.forEach(stopAnnotation => {
 	map.addAnnotation(stopAnnotation)
 })
-
-async function updateBuses() {
-	const busAnnotations = await api.readBuses()
-	const existingBusAnnotations = map.annotations.filter(annotation => {
-		return annotation.title.indexOf("Bus") == 0
-	})
-	map.removeAnnotations(existingBusAnnotations)
-	busAnnotations.forEach(busAnnotation => {
-		map.addAnnotation(busAnnotation)
-	})
-}
-
 window.setInterval(updateBuses, 5000)
 await updateBuses()
