106294130db976feadce2761caf1091575b6ea10
Gabriel Jacoby-Cooper <gerzersoftware@icloud.com>
2021-09-22 22:09:56 -0400

    Partial work towards impoving async compatibility

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,6 @@
+import * as asyncWrapper from "./async.js"
 import API from "./api.js"
 import Cookies from "./dist/js.cookie.min.mjs"
-
-async function updateBuses() {
-	const busAnnotations = await api.readBuses()
-	const existingBusAnnotations = map.annotations.filter(annotation => {
-		return annotation.title.indexOf("Bus") == 0
-	})
-	map.removeAnnotations(existingBusAnnotations)
-	busAnnotations.forEach(busAnnotation => {
-		map.addAnnotation(busAnnotation)
-	})
-}
 
 const prefersDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
 const bootstrapDarkmode = window['bootstrap-darkmode']
@@ -47,28 +37,5 @@ if (!welcomeModalShown) {
 	welcomeModal.show()
 }
 
-const tokenID = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ilg0M0szUjk0VDIifQ.eyJpc3MiOiJTWUJMSDI3N05GIiwiaWF0IjoxNjMwODU3NTYxLCJleHAiOjE2NjIzMzYwMDB9.ksSxblUOJECqgDaBlPU-VZsXG-UGI-W8ty8raDkD_eG2q6cW79-3EoDCHj-wHmynXRmDWR6Yfg72xknMS--ZdA"
-mapkit.init({
-	authorizationCallback: function(done) {
-		done(tokenID)
-	}
-})
-const center = new mapkit.Coordinate(42.73029109316892, -73.67655873298646)
-const span = new mapkit.CoordinateSpan(0.016, 0.016)
-const region = new mapkit.CoordinateRegion(center, span)
-const map = new mapkit.Map("map")
-map.showsUserLocation = true
-map.showsUserLocationControl = true
-map.tintColor = "red"
-map.mapType = mapkit.Map.MapTypes.MutedStandard
-map.colorScheme = prefersDarkMode ? mapkit.Map.ColorSchemes.Dark : mapkit.Map.ColorSchemes.Light
-map.setRegionAnimated(region)
-const api = new API()
-const routeOverlay = await api.readRoutes()
-map.addOverlay(routeOverlay)
-const stopAnnotations = await api.readStops()
-stopAnnotations.forEach(stopAnnotation => {
-	map.addAnnotation(stopAnnotation)
-})
-window.setInterval(updateBuses, 5000)
-await updateBuses()
+document.api = new API()
+await asyncWrapper.initializeMap(prefersDarkMode)
---
+++ scripts/noasync.js
@@ -0,0 +1,37 @@
+function updateBuses() {
+	const busAnnotations = document.api.readBuses()
+	const existingBusAnnotations = map.annotations.filter(annotation => {
+		return annotation.title.indexOf("Bus") == 0
+	})
+	map.removeAnnotations(existingBusAnnotations)
+	busAnnotations.forEach(busAnnotation => {
+		map.addAnnotation(busAnnotation)
+	})
+}
+
+function initializeMap() {
+	const tokenID = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ilg0M0szUjk0VDIifQ.eyJpc3MiOiJTWUJMSDI3N05GIiwiaWF0IjoxNjMwODU3NTYxLCJleHAiOjE2NjIzMzYwMDB9.ksSxblUOJECqgDaBlPU-VZsXG-UGI-W8ty8raDkD_eG2q6cW79-3EoDCHj-wHmynXRmDWR6Yfg72xknMS--ZdA"
+	mapkit.init({
+		authorizationCallback: function(done) {
+			done(tokenID)
+		}
+	})
+	const center = new mapkit.Coordinate(42.73029109316892, -73.67655873298646)
+	const span = new mapkit.CoordinateSpan(0.016, 0.016)
+	const region = new mapkit.CoordinateRegion(center, span)
+	const map = new mapkit.Map("map")
+	map.showsUserLocation = true
+	map.showsUserLocationControl = true
+	map.tintColor = "red"
+	map.mapType = mapkit.Map.MapTypes.MutedStandard
+	map.colorScheme = prefersDarkMode ? mapkit.Map.ColorSchemes.Dark : mapkit.Map.ColorSchemes.Light
+	map.setRegionAnimated(region)
+	const routeOverlay = document.api.readRoutes()
+	map.addOverlay(routeOverlay)
+	const stopAnnotations = document.api.readStops()
+	stopAnnotations.forEach(stopAnnotation => {
+		map.addAnnotation(stopAnnotation)
+	})
+	window.setInterval(updateBuses, 5000)
+	updateBuses()
+}
