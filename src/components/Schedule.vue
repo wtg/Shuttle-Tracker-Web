ad37c7f25e17d2c1f6585f89fc819d2e61b7dc6d
Jonathan Li <jonathanli50976@gmail.com>
2021-10-01 16:21:49 -0400

    schedual-barebone

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,11 +1,26 @@
 <template>
-<h1>
-  Schedule
-</h1>  
+  <div>
+    <h3 :class="{'text-white': isDarkMode}">
+      Fall 2021 Schedule
+    </h3>
+    <ul :class="{'text-white': isDarkMode}">
+      <li> Monday - Friday, 7:00 a.m. - 11:45 p.m </li>
+      <li> Saturday 9:00 a.m. - 11:45 p.m. </li>
+      <li> Sunday 9:00 a.m. - 8:00 p.m.  </li>
+    </ul>
+  </div>
 </template>
-<script>
 
+<script>
 export default {
-  name: 'Schedule'
+  computed: {
+    isDarkMode() {
+      return this.$store.state.isDarkMode
+    }
+  }
 }
 </script>
+
+<style scoped>
+
+</style>
