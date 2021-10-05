ece5d244ee1e8803b2bdf1b8c50abf3f70b81c3e
Jonathan Li <jonathanli50976@gmail.com>
2021-10-05 16:57:40 -0400

    added modal dark theme

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,10 @@
 <template>
-  <div :class="[{'text-white': isDarkMode}]">
-    <b-modal ref="instructionModal" ok-only ok-title="Continue" title="Welcome to the new Shuttle Tracker!">
-      <div> You're using the new Web interface for the <i> new </i> Shuttle Tracker. We've completely overhauled every component to bring superior accuracy. </div>
-      <div class="my-2"> Green Buses indicates accurate location data while red buses show low quality location data</div>
-      <div class="my-2"> If you use iPhone, then you can help out with crowd-sourced location data. Download our new beta app today! Click on "iOS App" link located on the bottom right hand corner of the map for more information.</div>
-      <div> <i> Our Android app is comming soon!</i></div>
+  <div class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]">
+    <b-modal class="wtitle" footer-bg-variant="dark" header-bg-variant ="dark" body-bg-variant="dark"  ref="instructionModal" ok-only ok-title="Continue" title="Welcome to the new Shuttle Tracker!">
+      <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> You're using the new Web interface for the <i> new </i> Shuttle Tracker. We've completely overhauled every component to bring superior accuracy. </div>
+      <div class="my-2" :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> Green Buses indicates accurate location data while red buses show low quality location data</div>
+      <div class="my-2" :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> If you use iPhone, then you can help out with crowd-sourced location data. Download our new beta app today! Click on "iOS App" link located on the bottom right hand corner of the map for more information.</div>
+      <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> <i> Our Android app is comming soon!</i></div>
     </b-modal>
   </div>
 </template>
@@ -39,6 +39,11 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style>
+.modal-override{
+    background: #0008;
+}
+.wtitle {
+  color: white;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,9 +6,7 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueCookies from 'vue-cookies'
 
-Vue.use(VueCookies)
 Vue.config.productionTip = false
 
 new Vue({
