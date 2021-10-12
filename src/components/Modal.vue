357ce9add607638a2605487c9f20e711c7dd9949
Isaac-the-Man <steven97102@gmail.com>
2021-10-12 15:27:24 -0400

    more code cleanup and grammar fix

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,18 +1,20 @@
 <template>
   <b-modal title="Welcome to the new Shuttle Tracker!" :header-text-variant="headerTextColor" :footer-bg-variant="theme"
            :header-bg-variant="theme" :body-bg-variant="theme" ref="instructionModal" ok-only ok-title="Continue">
-    <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> You're using the new Web interface for the <i>
-      new </i> Shuttle Tracker. We've completely overhauled every component to bring superior accuracy.
+    <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]">
+      <p> You're using the new Web interface for the <i>
+        new </i> Shuttle Tracker. We've completely overhauled every component to bring superior accuracy.
+      </p>
+      <p> Green Buses indicates accurate
+        location data while red buses show low quality location data. When Color-Blind Mode is toggled, a scope icon will
+        indicate high-quality location data.
+      </p>
+      <p> If you use iPhone, then you can
+        help out with crowd-sourced location data. Download our new beta app today! Click on "iOS App" link located on the
+        bottom right hand corner of the map for more information.
+      </p>
+      <p><i> Our Android app is coming soon!</i></p>
     </div>
-    <div class="my-2" :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> Green Buses indicates accurate
-      location data while red buses show low quality location data. When Color-Blind Mode is toggled, a scope icon will
-      indicate high-quality location data.
-    </div>
-    <div class="my-2" :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> If you use iPhone, then you can
-      help out with crowd-sourced location data. Download our new beta app today! Click on "iOS App" link located on the
-      bottom right hand corner of the map for more information.
-    </div>
-    <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"><i> Our Android app is comming soon!</i></div>
   </b-modal>
 </template>
 
