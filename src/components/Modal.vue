8c68b1ff654ba43a43b920cb479a3d7a0a49ab99
Jonathan Li <jonathanli50976@gmail.com>
2021-10-11 23:14:53 -0400

    working on darkmode

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]">
-    <b-modal class="wtitle" footer-bg-variant="dark" header-bg-variant ="dark" body-bg-variant="dark"  ref="instructionModal" ok-only ok-title="Continue" title="Welcome to the new Shuttle Tracker!">
-      <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> You're using the new Web interface for the <i> new </i> Shuttle Tracker. We've completely overhauled every component to bring superior accuracy. </div>
-      <div class="my-2" :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> Green Buses indicates accurate location data while red buses show low quality location data</div>
-      <div class="my-2" :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> If you use iPhone, then you can help out with crowd-sourced location data. Download our new beta app today! Click on "iOS App" link located on the bottom right hand corner of the map for more information.</div>
-      <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> <i> Our Android app is comming soon!</i></div>
-    </b-modal>
+  <div :class="[{'text-white': isDarkMode}]">
+  <b-modal title="Welcome to the new Shuttle Tracker!" :header-text-variant="textcolor" :footer-bg-variant="theme" :header-bg-variant="theme" :body-bg-variant="theme" ref="instructionModal" ok-only ok-title="Continue" >
+  <!--b-modal title="Welcome to the new Shuttle Tracker!" :header-text-variant="textcolor" :footer-bg-variant="theme" :header-bg-variant="theme" :body-bg-variant="theme" ref="instructionModal" ok-only ok-title="Continue"-->
+    <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> You're using the new Web interface for the <i> new </i> Shuttle Tracker. We've completely overhauled every component to bring superior accuracy. </div>
+    <div class="my-2" :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> Green Buses indicates accurate location data while red buses show low quality location data</div>
+    <div class="my-2" :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> If you use iPhone, then you can help out with crowd-sourced location data. Download our new beta app today! Click on "iOS App" link located on the bottom right hand corner of the map for more information.</div>
+    <div :class="[{'text-white': isDarkMode}, {'bg-dark':isDarkMode}]"> <i> Our Android app is comming soon!</i></div>
+  </b-modal>
   </div>
 </template>
 
@@ -14,26 +15,39 @@
 import Vue from 'vue'
 import VueCookies from 'vue-cookies'
 Vue.use(VueCookies)
-
 export default {
   name: "Modal",
-  mounted(){
-    if(!Vue.$cookies.isKey('shownInstructionModal')){
-      this.showModal()
-      Vue.$cookies.set('shownInstructionModal', 'shownInstructionModalValue')
-    }
-  },
-  methods: {
-    showModal() {
-      this.$refs['instructionModal'].show()
-    },
-    hideModal() {
-      this.$refs['instructionModal'].hide()
+  data() {
+    return{
+      theme: '',
+      textcolor: '',
     }
   },
   computed: {
     isDarkMode() {
       return this.$store.state.isDarkMode
+    },
+  },
+  mounted() {
+   // if(!Vue.$cookies.isKey('shownInstructionModal')){
+//      Vue.$cookies.set('shownInstructionModal', 'shownInstructionModalValue')
+    //}
+    this.showDarkMode()
+    this.showModal()
+  },
+  methods: {
+    showModal() {
+      this.$refs['instructionModal'].show()
+    },
+    showDarkMode(){
+      if(this.isDarkMode){
+        this.theme='dark'
+        this.textcolor='white'
+      }
+      else{
+        this.theme='white'
+        this.textcolor='dark'
+      }
     }
   },
 }
