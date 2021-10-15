1763c90d531c5f572e2140161d582368d47f09b4
ACE3R2 <bavrahamics@gmail.com>
2021-10-15 17:05:50 -0400

    Added an API check badge

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -3,6 +3,7 @@
     <b-badge class="mx-1" :variant="serverStatus.routes ? 'success': 'danger'" v-b-tooltip.hover :title="compStatus">Routes</b-badge>
     <b-badge class="mx-1" :variant="serverStatus.stops ? 'success': 'danger'" v-b-tooltip.hover :title="compStatus">Stops</b-badge>
     <b-badge class="mx-1" :variant="serverStatus.buses ? 'success': 'danger'" v-b-tooltip.hover :title="compStatus">Buses</b-badge>
+    <b-badge class="mx-1 ml-3" :variant="APIStatus() ? 'success': 'danger'" v-b-tooltip.hover :title="APIWarning">API</b-badge>
     <b-form-checkbox v-model="devHQ" name="check-button" switch :class="{'text-white': isDarkMode}">
       Create Fake HQ data: Bus ( ͡° ͜ʖ ͡°)
     </b-form-checkbox>
@@ -10,18 +11,29 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "Status",
   data() {
     return {
+      baseURL: 'https://shuttletracker.app',
       compStatus: "If the badge is green, this component is working!",
+      APIWarning: "If the badge is red, the app may be broken. You have been warned.",
       devHQ: false
     }
   },
   methods: {
     setHQData() {
-      console.log(this.devHQ)
       this.$store.commit('setFakeHQ', this.devHQ)
+    },
+    async APIStatus() {
+      try {
+        const res = await axios.get(this.baseURL + '/version')
+        return res.data == 0
+      } catch {
+        return false
+      }
     }
   },
   watch: {
