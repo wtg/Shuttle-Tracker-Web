ae30b9d981e26183f346163ed1ef313da35151e9
Ji Wang <wj42421999@gmail.com>
2021-10-15 16:49:24 -0400

    Revert "Add Font Awesome support"
    This reverts commit 1a545b2bd751e2ab7fc07d71873c62c0577082e8.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <link rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
-          integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
-          crossorigin="anonymous">
     <router-view/>
   </div>
 </template>
