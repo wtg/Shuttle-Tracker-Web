b7e716972d25a2da1e3ca8c3dd9b26e47ebaec34
Isaac-the-Man <steven97102@gmail.com>
2021-10-23 15:44:50 -0400

    move fakeHQdata switch to settings

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -4,9 +4,6 @@
     <b-badge class="mx-1" :variant="serverStatus.stops ? 'success': 'danger'" v-b-tooltip.hover :title="compStatus">Stops</b-badge>
     <b-badge class="mx-1" :variant="serverStatus.buses ? 'success': 'danger'" v-b-tooltip.hover :title="compStatus">Buses</b-badge>
     <b-badge class="mx-1" :variant="serverStatus.version ? 'success': 'danger'" v-b-tooltip.hover :title="APIWarning">API</b-badge>
-    <b-form-checkbox v-if="devToolsEnabled" v-model="devHQ" name="check-button" switch :class="{'text-white': isDarkMode}">
-      Create Fake HQ data: Bus 69
-    </b-form-checkbox>
   </div>
 </template>
 
@@ -17,18 +14,6 @@ export default {
     return {
       compStatus: "If the badge is green, this component is working!",
       APIWarning: "If the badge is red, the app may be broken. You have been warned.",
-      devHQ: false,
-      devToolsEnabled: process.env.VUE_APP_DEV_TOOLS_ENABLED === "true"
-    }
-  },
-  methods: {
-    setHQData() {
-      this.$store.commit('setFakeHQ', this.devHQ)
-    }
-  },
-  watch: {
-    devHQ () {
-      this.setHQData()
     }
   },
   computed: {
