d3ac6cd052eeb4457387c69bab3d6b5657ddb266
Isaac-the-Man <steven97102@gmail.com>
2021-10-23 16:28:57 -0400

    added expandable server status

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    <b-badge class="mr-1" :variant="serverStatus.routes ? 'success': 'danger'" v-b-tooltip.hover :title="compStatus">Routes</b-badge>
-    <b-badge class="mx-1" :variant="serverStatus.stops ? 'success': 'danger'" v-b-tooltip.hover :title="compStatus">Stops</b-badge>
-    <b-badge class="mx-1" :variant="serverStatus.buses ? 'success': 'danger'" v-b-tooltip.hover :title="compStatus">Buses</b-badge>
-    <b-badge class="mx-1" :variant="serverStatus.version ? 'success': 'danger'" v-b-tooltip.hover :title="APIWarning">API</b-badge>
+    <b-badge role="button" @click="toggleExpand" v-if="!expanded" class="mr-1"
+             :variant="totalServerStatus ? 'success': 'danger'" v-b-tooltip.hover :title="statusDescription">
+      Server: {{ totalServerStatus ? 'online' : 'offline' }}
+    </b-badge>
+    <div v-else>
+      <b-badge role="button" @click="toggleExpand" class="mx-1" :variant="serverStatus.routes ? 'success': 'danger'"
+               v-b-tooltip.hover :title="compStatus">
+        Routes
+      </b-badge>
+      <b-badge role="button" @click="toggleExpand" class="mx-1" :variant="serverStatus.stops ? 'success': 'danger'"
+               v-b-tooltip.hover :title="compStatus">
+        Stops
+      </b-badge>
+      <b-badge role="button" @click="toggleExpand" class="mx-1" :variant="serverStatus.buses ? 'success': 'danger'"
+               v-b-tooltip.hover :title="compStatus">
+        Buses
+      </b-badge>
+      <b-badge role="button" @click="toggleExpand" class="mx-1" :variant="serverStatus.version ? 'success': 'danger'"
+               v-b-tooltip.hover :title="APIWarning">
+        API
+      </b-badge>
+    </div>
   </div>
 </template>
 
@@ -12,16 +30,32 @@ export default {
   name: "Status",
   data() {
     return {
+      expanded: false,
       compStatus: "If the badge is green, this component is working!",
       APIWarning: "If the badge is red, the app may be broken. You have been warned.",
+      statusDescription: "Click to see detailed report on server status."
     }
   },
   computed: {
+    totalServerStatus() {
+      let status = true
+      Object.values(this.serverStatus).forEach((option) => {
+        if (!option) {
+          status = false
+        }
+      })
+      return status
+    },
     serverStatus() {
       return this.$store.state.serverStatus
     },
     isDarkMode() {
       return this.$store.state.isDarkMode
+    }
+  },
+  methods: {
+    toggleExpand() {
+      this.expanded = !this.expanded
     }
   }
 }
