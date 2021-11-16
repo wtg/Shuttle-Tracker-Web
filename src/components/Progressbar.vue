473ec930151cbb4d6b2c6442fa9f8e33e878eb3f
huynchu <chuhuyrpi@gmail.com>
2021-11-16 16:24:58 -0500

    added progress bar component

---
+++ src/components/Progressbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-progress :value=value :max=max></b-progress>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Progressbar",
+    data() {
+            return {
+                value: 35, // hard coded progres bar value
+                max: 100   // hard coded progress bar max value
+            }
+        },
+    computed: {
+        isDarkMode() {
+            return this.$store.state.isDarkMode
+        },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
