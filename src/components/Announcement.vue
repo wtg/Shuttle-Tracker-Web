ea337692fa9c996d9f0237943a54374e7d028315
Isaac-the-Man <steven97102@gmail.com>
2021-11-23 10:54:29 -0500

    smooth update of new announcement

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -1,24 +1,22 @@
 <template>
-  <div>
-    <b-alert
-        v-model="hasAnnouncements"
-        :class="[{'announcement-dark': isDarkMode}, {'announcement-light': !isDarkMode}]"
-        class="position-fixed fixed-top m-0 rounded-0 p-0 py-2 border-0"
-        style="z-index: 2000;">
-      <div ref="announcer" class="scroll-left">
-        <div class="position-absolute px-3" style="left: 0;top: 0;z-index: 2000;"
-             :class="[{'announcement-dark': isDarkMode}, {'announcement-light': !isDarkMode}]">📢:
-        </div>
-        <span class="scroll-text">
-          <span
-              class="mr-2 px-2"
-              :class="[{'announcement-dark': !isDarkMode}, {'announcement-light': isDarkMode}]">
-              {{ focusedAnnouncement.subject }}</span>
-            {{ focusedAnnouncement.body }}
-        </span>
+  <b-alert
+      v-model="hasAnnouncements"
+      :class="[{'announcement-dark': isDarkMode}, {'announcement-light': !isDarkMode}]"
+      class="position-fixed fixed-top m-0 rounded-0 p-0 py-2 border-0"
+      style="z-index: 2000;">
+    <div ref="announcer" class="scroll-left">
+      <div class="position-absolute px-3" style="left: 0;top: 0;z-index: 2000;"
+           :class="[{'announcement-dark': isDarkMode}, {'announcement-light': !isDarkMode}]">📢:
       </div>
-    </b-alert>
-  </div>
+      <span class="scroll-text">
+        <span
+            class="mr-2 px-2"
+            :class="[{'announcement-dark': !isDarkMode}, {'announcement-light': isDarkMode}]">
+            {{ focusedAnnouncement.subject }}</span>
+          {{ focusedAnnouncement.body }}
+      </span>
+    </div>
+  </b-alert>
 </template>
 
 <script>
@@ -28,48 +26,12 @@ export default {
   name: "Announcement",
   data() {
     return {
-      show: true,
+      updateOnNextInterval: false,
+      fetchInterval: parseInt(process.env.VUE_APP_ANNOUNCEMENT_UPDATE_INTERVAL), // update announcement every minute
       announcerIndex: 0,
       baseURL: process.env.VUE_APP_API_BASE_URL,
-      raw: [],
-      // raw: [
-      //   {
-      //     "end": "2022-11-21T03:33:05Z",
-      //     "start": "2021-11-20T03:33:05Z",
-      //     "id": "A1CB9E79-9511-48AC-B6A0-6F8B18D270FE",
-      //     "signature": "G5iAgVD7AFTmF7IQ==",
-      //     "subject": "Welcome (start and end)",
-      //     "body": "Welcome to the new Shuttle Tracker!",
-      //     "scheduleType": "startAndEnd"
-      //   },
-      //   {
-      //     "end": "2021-11-21T03:33:05Z",
-      //     "start": "2021-11-20T03:33:05Z",
-      //     "id": "0DF1E83F-4F7F-4C95-B32C-62D639341ED5",
-      //     "signature": "myxQ8H5eaEvI8a9JTTcetSG9muQ==",
-      //     "subject": "Test (end only)",
-      //     "body": "Testing testing 1...2...3...",
-      //     "scheduleType": "endOnly"
-      //   },
-      //   {
-      //     "end": "2021-11-21T03:33:05Z",
-      //     "start": "2021-11-20T03:33:05Z",
-      //     "id": "0DF1E83F-4F7F-4C95-B32C-62D639341ED5",
-      //     "signature": "myxQ8H5eaEvI8a9JTTcetSG9muQ==",
-      //     "subject": "NONE!!! (none)",
-      //     "body": "Oh yea oh yea",
-      //     "scheduleType": "none"
-      //   },
-      //   {
-      //     "end": "2021-11-21T03:33:05Z",
-      //     "start": "2021-11-20T03:33:05Z",
-      //     "id": "0DF1E83F-4F7F-4C95-B32C-62D639341ED5",
-      //     "signature": "myxQ8H5eaEvI8a9JTTcetSG9muQ==",
-      //     "subject": "NONE!!! (startOnly)",
-      //     "body": "Oh yea ohED5 0DF1E83F-4F7F-4C95-B32C-62D639341ED5 0DF1E83F-4F7F-4C95-B32C-62D639341ED5",
-      //     "scheduleType": "startOnly"
-      //   }
-      // ]
+      rawUpdate: [],  // temporarily stores updated announcements
+      raw: []
     }
   },
   computed: {
@@ -101,6 +63,25 @@ export default {
     }
   },
   methods: {
+    async updateAnnouncements() {
+      const vm = this
+      setInterval(async () => {
+        //  Fetch raw data from the announcement API
+        const res = await axios.get(vm.baseURL + '/announcements')
+        vm.rawUpdate = res.data
+        // compare updates
+        const rawIds = vm.raw.map(x => x.id)
+        const updateIds = vm.rawUpdate.map(x => x.id)
+        rawIds.sort()
+        updateIds.sort()
+        if (rawIds.length === updateIds.length && rawIds.every((v,i) => v === updateIds[i])) {
+          // nothing updated, wait for another cycle
+          return
+        }
+        // something is updated
+        vm.updateOnNextInterval = true
+      }, this.fetchInterval)
+    },
     async getAnnouncements() {
       //  Fetch raw data from the announcement API
       const res = await axios.get(this.baseURL + '/announcements')
@@ -112,6 +93,13 @@ export default {
     loadAnnouncer() {
       //  Load different announcement for every animation loop
       this.$refs.announcer.addEventListener('animationiteration', () => {
+        if (this.updateOnNextInterval) {
+          // update announcement
+          this.raw = this.rawUpdate
+          this.rawUpdate.clear()
+          this.updateOnNextInterval = false
+        }
+        // cycle displayed announcement
         this.announcerIndex++
         if (this.announcerIndex >= this.announcements.length) {
           this.announcerIndex = 0
@@ -121,6 +109,7 @@ export default {
   },
   mounted() {
     this.getAnnouncements()
+    this.updateAnnouncements()
   }
 }
 </script>
