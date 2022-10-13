708f023ce9836ad27ebd6f1161656a9ac086de8c
Gabriel Jacoby-Cooper <gerzersoftware@icloud.com>
2022-10-13 19:31:30 -0400

    Add contact information to privacy policy

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -13,6 +13,9 @@
             <p>
               Shuttle Tracker sends your location data to our server only when you tap “Board Bus” and stops sending these data when you tap “Leave Bus”. Your location data are associated with an anonymous, random identifier that rotates every time you start a new shuttle trip. These data aren’t associated with your name, email address, Apple ID, RCS ID, Google account, or any other identifying information. We continuously purge location data that are more than 30 seconds old from our server. We may retain resolved location data that are calculated using a combination of system- and user-reported data indefinitely, but these resolved data don’t correspond with any specific user-reported coordinates.
             </p>
+            <p>
+              Shuttle Tracker is developed as a partnership between the Web Technologies Group and the <a href="https://rcos.io">Rensselaer Center for Open Source</a> at <a href="https://rpi.edu">Rensselaer Polytechnic Institute</a>. You can <a href="mailto:privacy@shuttletracker.app">contact our privacy team</a> with inquiries about this policy and the use of your data.
+            </p>
             <b-button to="/"> Home Page </b-button>
           </div>
         </div>
