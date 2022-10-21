262e9b48f1ed989b25c44691ed016e1582dca6ac
lij47 <lij47@rpi.edu>
2022-10-21 16:24:28 -0400

    updated pagenotfound page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -8,9 +8,9 @@
       </div>
       <div>
         <div class="row">
-          <div class="col">
-            <h2 class="text-white">Error 404</h2>
-            <p class="text-white">
+          <div class="col" :class="{'text-white': isDarkMode}">
+            <h2>Error 404</h2>
+            <p>
                 Page Not Found
             </p>
             <b-button to="/"> Home Page </b-button>
@@ -23,9 +23,11 @@
 
 <script>
 import Header from "../components/Header";
+import mixin from "../mixins/mixins";
 
 export default {
   name: "PageNotFound",
+  mixins: [mixin],
   components: {
     Header
   }
