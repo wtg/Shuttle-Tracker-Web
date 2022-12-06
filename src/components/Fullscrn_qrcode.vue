9dd74bf69cee784f145ca86df07bc9e94e644d70
Isaac-the-Man <steven97102@gmail.com>
2022-12-06 17:05:24 -0500

    rotate qr-codes and new android qr-code

--- src/components/Fullscrn_qrcode.vue
+++ src/components/Fullscrn_qrcode.vue
@@ -4,11 +4,15 @@
     <h3 :class="{ 'text-white': isDarkMode }">
       Get The Mobile App!
     </h3>
-    <div class="text-center">
-      <div :class="{ 'text-white': isDarkMode }">iOS</div>
-      <img src="../../public/App_Clip_Code.svg" alt="ios qr code" class="qr-code"/>
-      <div :class="{ 'text-white': isDarkMode }">Android</div>
-      <img src="../../public/Android_qr.svg" alt="android qr code" class="qr-code"/>
+    <div class="text-center qr-codes">
+      <div v-if="qr_id === 0">
+        <div :class="{ 'text-white': isDarkMode }">iOS</div>
+        <img src="../../public/App_Clip_Code.svg" alt="ios qr code" class="qr-code"/>
+      </div>
+      <div v-else>
+        <div :class="{ 'text-white': isDarkMode }">Android</div>
+        <img src="../../public/Android_Clip_Code.png" alt="android qr code" class="qr-code"/>
+      </div>
     </div>
   </b-card>
 </template>
@@ -21,13 +25,25 @@ export default {
   mixins: [mixin],
   data() {
     return {
-      showQR: false
+      showQR: false,
+      swap_interval: 5000, // interval at which the QR codes are swapped (milliseconds)
+      qr_id: 0
     }
   },
   watch: {
     '$store.state.showIcons': function () {
       this.showQR = this.$store.state.showIcons;
     }
+  },
+  mounted() {
+    // Swap QR-codes
+    setInterval(() => {
+      if (this.qr_id === 0) {
+        this.qr_id = 1
+      } else {
+        this.qr_id = 0
+      }
+    }, this.swap_interval)
   }
 };
 </script>
@@ -35,11 +51,7 @@ export default {
 <style scoped>
 
 .qr-code {
-  width: 70%;
-}
-
-.flex-even {
-  flex: 1;
+  width: 15rem;
 }
 
 .bubble-light {
