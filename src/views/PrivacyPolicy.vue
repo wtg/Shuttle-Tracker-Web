962aebc7675a552ed3b8382fec7ab4c301238383
yZzzC <105983201+yZzzC@users.noreply.github.com>
2023-11-07 21:59:48 -0500

    Update PrivacyPolicy.vue
    Added arrow to indicate if a collapsible is opened.

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -21,19 +21,26 @@
               <a href="https://new.rcos.io">Rensselaer Center for Open Source</a> at <a href="https://rpi.edu">
                 Rensselaer Polytechnic Institute</a>. You can <a href="mailto:privacy@shuttletracker.app">contact our privacy team</a> with inquiries about this policy and the use of your data.
             </p>
-            <button class="collapsible" :class="{'text-white': isDarkMode}" @click="toggleCollapsible(0)">Location</button>
+            <button class="collapsible" :class="{'text-white': isDarkMode, 'open': isCollapsibleOpen[0]}" @click="toggleCollapsible(0)">
+              Location
+              <span class="arrow" :class="{'rotate': isCollapsibleOpen[0]}">▶</span>
+            </button>
             <div class="content" :class="{'show': isCollapsibleOpen[0]}">
             <p>
               Shuttle Tracker sends your location data to our server only when Board Bus is activated and stops sending these data when Board Bus is deactivated. You can activate Board Bus manually by tapping “Board Bus” or automatically by positioning your device within Bluetooth range of a Shuttle Tracker Node device on a bus if you opted in to the Shuttle Tracker Network. You can deactivate Board Bus manually by tapping “Leave Bus” or automatically by positioning your device out of Bluetooth range of a Shuttle Tracker Node device on a bus if you opted in to the Shuttle Tracker Network. Your location data are associated with an anonymous, random identifier that rotates every time you start a new shuttle trip. These data aren’t associated with your name, Apple ID, RCS ID, RIN, or any other information that might identify you or your device. We continuously purge location data that are more than 30 seconds old from our server. We may retain resolved location data that are calculated using a combination of system- and user-reported data indefinitely, but these resolved data don’t correspond with any specific user-reported coordinates. Even if you opt in to the Shuttle Tracker Network, we never track your location unless you manually activate Board Bus or physically board a bus. Your device might alert you to Shuttle Tracker’s location monitoring in the background even when Shuttle Tracker isn’t actually tracking your location. This is due to a system limitation; Shuttle Tracker occasionally scans for Shuttle Tracker Node devices in the  background, and your device might show that activity as location tracking. The results of these scans never leave your device, and we only start collecting location data if a scan indicates that you’re physically on a bus.
             </p>
             </div>
-            <button class="collapsible" :class="{'text-white': isDarkMode}" @click="toggleCollapsible(1)">Logging</button>
+            <button class="collapsible" :class="{'text-white': isDarkMode}" @click="toggleCollapsible(1)">Logging
+              <span class="arrow" :class="{'rotate': isCollapsibleOpen[1]}">▶</span>
+            </button>
             <div class="content" :class="{'show': isCollapsibleOpen[1]}">
             <p>
               Shuttle Tracker automatically detects errors and uploads diagnostic logs to our server when they occur. These logs aren’t associated with your name, Apple ID, RCS ID, RIN, or any other information that might identify you or your device. They contain information about, for example, failed network requests. We redact sensitive information like your location, replacing those data with irreversible hashes. These hashes let us correlate different logs without revealing any of the redacted information. Logs are retained indefinitely; contact us if you want to request that we delete a log from our server. Due to the privacy-preserving nature of how we identify logs, we might not be able to find and to verify the log that you want to delete. You can see a record of recently uploaded logs or disable automatic uploads entirely in Settings > Logging & Analytics.
             </p>
             </div>
-            <button class="collapsible" :class="{'text-white': isDarkMode}" @click="toggleCollapsible(2)">Analytics</button>
+            <button class="collapsible" :class="{'text-white': isDarkMode}" @click="toggleCollapsible(2)">Analytics
+              <span class="arrow" :class="{'rotate': isCollapsibleOpen[2]}">▶</span>
+            </button>
             <div class="content" :class="{'show': isCollapsibleOpen[2]}">
             <p>
               If you opt in to analytics, then Shuttle Tracker will send anonymous usage data to our server. These data include your app settings, feature usage frequency, and other similar metrics. No analytics data are ever collected unless you explicitly opt in. You can see a record of recently uploaded analytics reports or opt-in status in Settings > Logging & Analytics.
@@ -49,6 +56,7 @@
     </div>
   </div>
 </template>
+
 
 <script>
 import mixin from "../mixins/mixins";
@@ -77,6 +85,7 @@ export default {
 </script>
 
 <style scoped>
+
 .collapsible {
   background-color: transparent; 
   cursor: pointer;
@@ -84,27 +93,42 @@ export default {
   width: 100%;
   border: none;
   border-top: 1px solid;
-  padding-top: 10px;
+  padding-top: 9px;
+  padding-left: 5px;
   padding-bottom: 10px;
   text-align: left;
   outline: none;
   font-size: 30px;
   transition: background-color 0.3s, color 0.3s;
+  position: relative;
+}
+.arrow {
+  font-family: 'Glyphicons Halflings';
+  position: absolute;
+  right: 10px;
+  transition: transform 0.3s;
+}
+
+.rotate {
+  -webkit-transform: rotate(90deg);
+	-moz-transform: rotate(90deg);
+	transform: rotate(90deg);
 }
 .content {
   padding: 0 18px;
   max-height: 0;
   overflow: hidden;
-  transition: max-height 0.5s ease-out;
+  transition: all 0.5s ease-out;
 }
 .show {
   padding: 0 18px;
   max-height: 2000px;
   overflow: visible;
-  transition: max-height 0.5s ease-in;
+  transition: all 0.5s ease-in;
 }
+
 .collapsible:hover {
-  background-color: #5555555b;
+  background-color: #5555553a;
 }
 .custom-bullet {
   position: relative;
