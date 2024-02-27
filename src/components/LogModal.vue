d74fe6ae7bf5df7d1079de39c4d12bc404802301
michaelni678 <michael.ni678@gmail.com>
2024-02-27 16:58:40 -0500

    comments and some api renames

--- src/components/LogModal.vue
+++ src/components/LogModal.vue
@@ -16,6 +16,8 @@
       <link rel="stylesheet' href='css/bootstrap.min.css" />
     </head>
     <div :class="[{ 'text-white': isDarkMode }, { 'bg-dark': isDarkMode }]">
+      <!-- If there are pages, display a paginated table. -->
+      <!-- If there are no pages, display a placeholder text instead of the table. -->
       <div v-if="logTablePages.length > 0">
         <table class="log-table">
           <thead>
@@ -27,15 +29,21 @@
           </thead>
           <tbody>
             <tr v-for="log in logTablePages[currentPage - 1]" :key="log.uuid">
+              <!-- Format the date according to `dateFormat`. -->
               <td>{{ log.date.toLocaleString('en-US', dateFormat) }}</td>
+              <!-- The uploaded log's UUID, returned by the server upon upload. -->
               <td>{{ log.uuid }}</td>
+              <!-- 
+                  The log's contents. Note that log.content is a string possibly containing newlines.
+                  The `pre-wrap` style is to preserve those newlines.
+              -->
               <td>
                 <div style="white-space: pre-wrap;">{{ log.content }}</div>
               </td>
             </tr>
           </tbody>
         </table>
-        <div class="pagination-container">
+        <div class="log-table-pagination">
           <b-pagination v-model="currentPage" :total-rows="uploadedLogs.length" :per-page="perPage" />
         </div>
       </div>
@@ -51,8 +59,11 @@ export default {
   name: "Modal",
   data() {
     return {
+      // The formatting options for the date displayed in the log table.
       dateFormat: { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false },
+      // The number of uploaded logs (rows) per page.
       perPage: 3,
+      // The current page of the pagination (1-indexed).
       currentPage: 1,
     };
   },
@@ -63,6 +74,12 @@ export default {
     isColorBlind() {
       return this.$store.state.isCbMode;
     },
+    themeBg() {
+      return this.isDarkMode ? "dark" : "light";
+    },
+    themeText() {
+      return this.isDarkMode ? "light" : "dark";
+    },
     /// Check if the log modal is currently visible.
     logModalVisible() {
       return this.$store.state.logModalVisible;
@@ -71,19 +88,7 @@ export default {
     uploadedLogs() {
       return this.$store.state.logBuffer.uploads;
     },
-    themeBg() {
-      return this.isDarkMode ? "dark" : "light";
-    },
-    themeText() {
-      return this.isDarkMode ? "light" : "dark";
-    },
-    logModalTableFields() {
-      return [
-        { key: 'date', label: 'Date' },
-        { key: 'uuid', label: 'UUID' },
-        { key: 'content', label: 'Content' }
-      ];
-    },
+    /// The pages of the log table. The number of rows per page are dictated by `perPage`.
     logTablePages() {
       const chunks = [];
       for (let i = 0; i < this.uploadedLogs.length; i += this.perPage) {
@@ -115,6 +120,8 @@ export default {
 </script>
 
 <style>
+/* Log table styles. */
+
 .log-table {
   border-collapse: collapse;
   width: 100%;
@@ -130,20 +137,22 @@ export default {
   background-color: #f2f2f2;
 }
 
-.pagination-container {
+/* Log table pagination styles. */
+
+.log-table-pagination {
   display: flex;
   justify-content: center;
   margin-top: 20px;
 }
 
-.pagination-container .page-item {
+.log-table-pagination .page-item {
   display: inline-block;
   margin-right: 5px;
   border: 1px solid #ddd;
   border-radius: 3px;
 }
 
-.pagination-container .page-link {
+.log-table-pagination .page-link {
   padding: 6px 12px;
   color: #007bff;
   background-color: #fff;
@@ -151,11 +160,11 @@ export default {
   border-radius: 3px;
 }
 
-.pagination-container .page-link:hover {
+.log-table-pagination .page-link:hover { /* Make hovered page gray. */
   background-color: #f0f0f0;
 }
 
-.pagination-container .page-item.active .page-link {
+.log-table-pagination .page-item.active .page-link { /* Make current page on pagination blue. */
   background-color: #007bff;
   color: #fff;
   border-color: #007bff;
